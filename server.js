fa04ed8259629de03653280c09e0e540d32874f5
mayar <mayaripaad@gmail.com>
2026-07-07 01:48:03 +0300

    fix CORS for production

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: ['https://glim-store.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}))
 app.use(express.json())
 const productRoutes = require('./routes/productRoutes')
 app.use('/api/products', productRoutes)
